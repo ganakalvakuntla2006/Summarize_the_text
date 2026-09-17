034fb2a683c4091bd521ec1eb7744c6cc7ff953a
ganakalvakuntla2006 <ganakalvakuntla@gmail.com>
2026-09-17 17:33:44 +0530

    test: fix CONFIG_FILE_PATH assertion in test_pipeline.py

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,11 +1,12 @@
 import os
 from textSummarizer.config.configuration import ConfigurationManager
+from textSummarizer.constants import CONFIG_FILE_PATH
 
 def test_config_manager():
     config_mgr = ConfigurationManager()
     ingestion_config = config_mgr.get_data_ingestion_config()
     assert ingestion_config is not None
-    assert os.path.exists(config_mgr.config_filepath)
+    assert os.path.exists(CONFIG_FILE_PATH)
 
 def test_data_validation_config():
     config_mgr = ConfigurationManager()
